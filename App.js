8aad7e253e855b9c6b5f9963e1a729a18e2b8a02
Petrus Hurtig <petrushurtig@hotmail.com>
2021-01-29 13:18:25 +0200

    Added database

--- App.js
+++ App.js
@@ -1,39 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('shoppinglistdb.db');
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [text, setText] = useState();
-  const add = () => {
-    setData([...data, {key: text}]);
-    setText();
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [shoppinglist, setShoppinglist] = useState([]);
+
+  const initialFocus = useRef(null);
+  const clear = useRef(null);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppinglist (id integer primary key not null, product text, amount text);');
+    }, null, updateList);
+}, []);
+
+  const saveItem = () => {
+    db.transaction(tx => {
+      tx.executeSql('insert into shoppinglist (product, amount) values (?, ?);', [product, amount]);
+    }, null, updateList
+    )
+    
   }
-  const clear = () => {
-    setData([])
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppinglist;', [], (_, { rows }) => 
+      setShoppinglist(rows._array)
+      );
+    });
+    initialFocus.current.focus();
+    initialFocus.current.clear();
+    clear.current.clear();
   }
+
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql('delete from shoppinglist where id = ?;', [id]);
+      }, null, updateList
+    )
+  }
+  
+  const listSeparator = () => {
+    return (
+      <View
+      style={{
+        height: 5,
+        width: '80%',
+        backgroundColor: '#fff',
+        marginLeft: '10%'
+      }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
-      onChangeText={text => setText(text)}
-      value={text}
+      ref={initialFocus}
+      placeholder='Product'
+      onChangeText={(product) => setProduct(product)}
+      value={product}
       style={{
-        width: 200, 
-        height:45, 
-        margin: 20, 
-        borderColor: 'black', 
-        borderWidth:1}} />
+       marginTop:30,
+       fontSize: 18,
+       width: 200,
+       borderColor:'gray',
+       borderWidth: 1}} />
+       <TextInput
+       ref={clear}
+       placeholder='Amount'
+       style={{
+        marginTop: 5,
+        marginBottom: 5, fontSize:18,
+        width: 200,
+        borderColor:'gray',
+        borderWidth:1}}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount}
+       />
       <View style={{flexDirection:'row'}}>
-      <Button title="Add" onPress={add}></Button>
-      <Button title="Clear" onPress={clear}></Button>
+      <Button title="Add" onPress={saveItem}></Button>
       </View>
-      <Text style={{marginTop: 30, fontSize:20, fontWeight: 'bold', color: 'blue'}}>Shopping List</Text>
-      <FlatList data={data} renderItem={({item}) => <Text>{item.key}</Text>}/>
-    
+      <Text style={{marginTop: 30, fontSize:20, fontWeight: 'bold', color: 'blue'}}>Items to buy</Text>
+      <FlatList 
+      style={{marginLeft: '5%'}}
+      keyExtractor={item => item.id.toString()}
+      data={shoppinglist}
+      ItemSeparatorComponent={listSeparator}
+      renderItem={({item}) => <View style={styles.listContainer}><Text style={{fontSize: 20}}>{item.product}, {item.amount}</Text>
+      <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> Bought</Text></View>}
+      />
       <StatusBar style="auto" />
     </View>
   );
-      }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -43,4 +108,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: 'blue',
+    borderTopWidth: 1
+  }
 });
